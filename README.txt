fb2c9a76077f4de99692f06ccc09b7b7cad3386b
Oscar Henrichs <oscar.henrichs.work@outlook.com>
2025-05-23 15:21:34 -0300

    readme

--- README.txt
+++ README.txt
@@ -1,1 +1,22 @@
-SDAD
+
+# API RANKING
+
+Para rodar poder usar o comando 
+
+```bash
+sudo docker-compose down -v && docker-compose up -d --build
+```
+
+ou 
+
+```bash
+sudo php -S 0.0.0.0:5000 src/index.php
+```
+
+e chamar usando
+
+```bash
+curl -X GET http://localhost:8080/ranking/?movimento_id=1
+```
+
+
--- src/index.php
+++ src/index.php
@@ -1,14 +1,14 @@
 <?php
-// phpinfo();
-// try {
-//     require_once __DIR__ . "/api/server.php";
-// } catch (Throwable $e) {
-//     header('Content-Type: application/json');
-//     http_response_code(500);
-//     echo json_encode([
-//         'error' => 'Internal Server Error',
-//         'message' => $e->getMessage(),
-//         'trace' => $e->getTrace() // Only in development!
-//     ]);
-//     exit;
-// }
+
+try {
+    require_once __DIR__ . "/api/server.php";
+} catch (Throwable $e) {
+    header('Content-Type: application/json');
+    http_response_code(500);
+    echo json_encode([
+        'error' => 'Internal Server Error',
+        'message' => $e->getMessage(),
+        'trace' => $e->getTrace() // Only in development!
+    ]);
+    exit;
+}
